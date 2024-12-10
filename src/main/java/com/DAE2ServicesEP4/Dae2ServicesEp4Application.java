557b15913ad19e5df1f8ac7dac2f0bd1d798b22a
CerocuelCode <CuelCipherX@outlook.com>
2024-12-10 13:50:01 -0500

    Creación del Proyecto
    Creamos el proyecto con Spring.

---
+++ src/main/java/com/DAE2ServicesEP4/Dae2ServicesEp4Application.java
@@ -0,0 +1,13 @@
+package com.DAE2ServicesEP4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dae2ServicesEp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dae2ServicesEp4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/DAE2ServicesEP4/Dae2ServicesEp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAE2ServicesEP4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dae2ServicesEp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
